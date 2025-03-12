0d706bfd613e648254bef4d1bcac13307dd60f3d
w33bvGL <w33bv.gl@gmail.com>
2025-03-12 20:03:29 +0400

    remove unused content and add nuxt base app

---
+++ app.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    the anidzen application tempalte
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,39 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxt/ui-pro',
+  ],
+
+  devtools: {
+    enabled: true
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  mdc: {
+    highlight: {
+      noApiRoute: false
+    }
+  },
+
+  routeRules: {
+    '/': { prerender: true }
+  },
+
+  future: {
+    compatibilityVersion: 4
+  },
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  },
+
+  compatibilityDate: '2025-01-15',
+})
