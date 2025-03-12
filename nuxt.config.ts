e5eb14123567bec2145464d67040b7b57e7089d6
w33bvGL <w33bv.gl@gmail.com>
2025-03-12 22:09:48 +0400

    generate pwa assets

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,27 +27,4 @@ export default defineNuxtConfig({
     }
   },
 
-  pwa: {
-    mode: 'development',
-    strategies: 'generateSW',
-    registerType: 'autoUpdate',
-    manifest: {
-      name: 'Nuxt Vite PWA',
-      short_name: 'NuxtVitePWA',
-      theme_color: '#ffffff'
-    },
-    pwaAssets: {
-      config: true
-    },
-    workbox: {},
-    client: {
-      installPrompt: true
-    },
-    devOptions: {
-      enabled: true,
-      suppressWarnings: true,
-      navigateFallback: '/',
-      navigateFallbackAllowlist: [/^\/$/]
-    }
-  }
 })
