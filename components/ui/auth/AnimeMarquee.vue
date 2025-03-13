30d4bdf9d0767fe3c247f2d6860b8310c9f02cb1
Vahe Sargsyan <w33bv.gl@gmail.com>
2025-03-13 19:11:58 +0400

    ✨ feat(auth.vue): Added a new div element to the auth layout.
    ✨ feat(AnimeMarquee.vue): Updated image URLs and added width to NuxtImg.

--- components/ui/auth/AnimeMarquee.vue
+++ components/ui/auth/AnimeMarquee.vue
@@ -1,17 +1,29 @@
 <script setup lang="ts">
 const imagesOne = ref([
-  'https://cdn.myanimelist.net/images/anime/1015/138006.jpg'
+  'https://cdn.myanimelist.net/images/anime/1015/138006.webp',
+  'https://cdn.myanimelist.net/images/anime/1245/116760.webp',
+  'https://cdn.myanimelist.net/images/anime/1122/96435.webp',
+  'https://cdn.myanimelist.net/images/anime/10/51723.webp',
+  'https://cdn.myanimelist.net/images/anime/1825/110716.webp',
+  'https://cdn.myanimelist.net/images/anime/1379/145452.webp',
+  'https://cdn.myanimelist.net/images/anime/1379/145452.webp',
+  'https://cdn.myanimelist.net/images/anime/5/87048.webp',
+  'https://cdn.myanimelist.net/images/anime/1084/112813.webp',
+  'https://cdn.myanimelist.net/images/anime/6/79597.webp'
 ])
 </script>
 
 <template>
   <div>
-    <UPageMarquee reverse>
+    <UPageMarquee
+      reverse
+      :ui="{ root: '[--gap:--spacing(4)]', content: 'w-auto py-1' }"
+    >
       <NuxtImg
         v-for="(image, index) in imagesOne"
         :key="index"
         :src="image"
-        class="pointer-events-none select-none"
+        class="pointer-events-none select-none w-[200px]"
       />
     </UPageMarquee>
   </div>
