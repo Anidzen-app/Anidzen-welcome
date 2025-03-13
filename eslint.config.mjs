8b6f548564e8c4770e218d49e6d336e2f08df016
w33bvGL <w33bv.gl@gmail.com>
2025-03-13 10:06:21 +0400

    add linters and other format

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
-import withNuxt from "./.nuxt/eslint.config.mjs";
+import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt([
   {
-    ignores: ["node_modules/", ".nuxt/", ".output/", "dist/", "ui-pro-plugin/"],
-  },
-]);
+    ignores: ['node_modules/', '.nuxt/', '.output/', 'dist/', 'ui-pro-plugin/']
+  }
+])
