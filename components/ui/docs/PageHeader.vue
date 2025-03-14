648675426ae03eead2b07974d3dc8ea645c2a676
Vahe Sargsyan <w33bv.gl@gmail.com>
2025-03-14 20:54:44 +0400

    add page header privacy policies

--- components/ui/docs/PageHeader.vue
+++ components/ui/docs/PageHeader.vue
@@ -1,9 +1,31 @@
 <script setup lang="ts">
-
+const props = defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  lastModified: {
+    type: String,
+    required: true
+  }
+})
 </script>
 
 <template>
-  <div />
+  <div>
+    <UPageHeader
+      :title="props.title"
+      :description="props.description"
+    >
+      <template #headline>
+        последное обнавление в {{ props.lastModified }}
+      </template>
+    </UPageHeader>
+  </div>
 </template>
 
 <style scoped>
