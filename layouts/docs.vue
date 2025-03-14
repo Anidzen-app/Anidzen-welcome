a5b61deabd908af098f3ae9ff509f91a6a02a06c
Vahe Sargsyan <w33bv.gl@gmail.com>
2025-03-14 19:37:42 +0400

    add forgot password fix and new password components

---
+++ layouts/docs.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <UContainer>
+      <UCard class="mt-10">
+        <slot />
+      </UCard>
+    </UContainer>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
