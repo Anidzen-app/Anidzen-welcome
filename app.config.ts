1399e0ff752bcce993dcc41e15ce1a00526b7acc
Vahe Sargsyan <w33bv.gl@gmail.com>
2025-03-20 12:17:44 +0400

    add packages and most

--- app.config.ts
+++ app.config.ts
@@ -3,6 +3,30 @@ export default defineAppConfig({
     colors: {
       primary: 'red',
       neutral: 'neutral'
+    },
+    toast: {
+      slots: {
+        root: 'relative group overflow-hidden bg-(--ui-bg) shadow-lg rounded-[calc(var(--ui-radius)*2)] ring ring-(--ui-border) p-4flex gap-2.5 focus:outline-none',
+        wrapper: 'w-0 flex-1 flex flex-col',
+        title: 'text-base font-medium text-(--ui-text-highlighted)',
+        description: 'text-sm text-(--ui-text-muted)',
+        icon: 'shrink-0 size-5',
+        avatar: 'shrink-0',
+        avatarSize: '2xl',
+        actions: 'flex gap-1.5 shrink-0',
+        progress: 'absolute inset-x-0 bottom-0 h-1 z-[-1]',
+        close: 'p-0'
+      },
+      orientation: {
+        horizontal: {
+          root: 'items-center',
+          actions: 'items-center'
+        },
+        vertical: {
+          root: 'items-start',
+          actions: 'items-start mt-2.5'
+        }
+      }
     }
   }
 })
